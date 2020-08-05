3f5066d07dd395ebee7e862b672f729cdd906639
megadose <51822495+megadose@users.noreply.github.com>
2020-08-05 11:38:09 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cqfd',
-    version="1.01",
+    version="1.011",
     packages=find_packages(),
     author="megadose",
     install_requires=["argparse","fake_useragent"],
